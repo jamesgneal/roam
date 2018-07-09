9fe8427933661d50fbfc85dbb8e5d82fed7c2441
Unknown <cgzoghby@gmail.com>
2018-07-09 19:33:39 -0400

    Please Build

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const session = require('express-session')
-const dbConnection = require('./database') 
+const dbConnection = require('./server/database') 
 const MongoStore = require('connect-mongo')(session)
-const passport = require('./passport');
+const passport = require('./server/passport');
 const app = express()
 const PORT = process.env.PORT || 8080
 // Route requires
-const user = require('./routes/api/user')
-const yelpLocation = require('./routes/api/locations')
-const routes = require('./routes')
+const user = require('./server/routes/api/user')
+const yelpLocation = require('./server/routes/api/locations')
+const routes = require('./server/routes')
 
 // MIDDLEWARE
 app.use(morgan('dev'))
