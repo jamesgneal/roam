1f207250132d17bf92ea6e843e575ae2a20f78ec
Christopher Zoghby <cgzoghby@gmail.com>
2018-07-09 21:03:06 -0400

    tweaking pathing

--- server.js
+++ server.js
@@ -44,9 +44,9 @@ app.use(routes)
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
+/* app.get('*', (req, res) => {
 	res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
+  }); */
 
 // Starting Server 
 app.listen(PORT, () => {
