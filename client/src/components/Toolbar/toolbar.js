611112e7664686857d0a832dd55045b8769167d8
Graham Neal <nealjg@gmail.com>
2018-07-17 18:11:31 -0400

    removed ToolbarFeatures bug, reverted to previous version. Updated styling for map, pin button

--- client/src/components/Toolbar/toolbar.js
+++ client/src/components/Toolbar/toolbar.js
@@ -1,67 +1,89 @@
-import React, { Component } from "react";
-import Button from "mdbreact";
+import React from "react";
+import {
+  Button,
+  Fa
+} from "mdbreact";
 import "./toolbar.css";
 
-class Toolbar extends Component {
+class ToolbarFeatures extends React.Component { 
   render() {
     return (
       <div>
-        <div className="background toolbar">
+        <div className="background modal-container">
           <div className="toolbar">
-            <Button
-              className="btn-circle"
-              onClick={e => this.props.catClick("restaurants", e)}
-            >
-            asdf
-              {/* <MaterialIcon icon="restaurant" invert /> */}
+            <Button className="btn-circle" onClick={(e) => this.props.catClick("breweries", e)}>
+              <Fa icon="beer" />
             </Button>
-            <Button
-              className="btn-circle"
-              onClick={e => this.props.catClick("bars", e)}
-            >
-            asdf
-              {/* <MaterialIcon icon="local_bar" invert /> */}
+            {/* <Button className="btn-circle">
+              <Fa icon="shopping-bag" />
+            </Button> */}
+            <Button className="btn-circle" onClick={(e) => this.props.catClick("restaurants", e)}>
+              <Fa icon="apple" id="apple" />
             </Button>
-            <Button
-              className="btn-circle"
-              onClick={e => this.props.catClick("coffee", e)}
-            >
-            asdf
-              {/* <MaterialIcon icon="local_cafe" invert /> */}
+            <Button className="btn-circle" onClick={(e) => this.props.catClick("local flavor", e)}>All</Button>
+            <Button className="btn-circle" onClick={(e) => this.props.catClick("concert venues", e)}>
+              <Fa icon="headphones" />
             </Button>
-            <Button
-              className="btn-circle"
-              onClick={e => this.props.catClick("local flavor", e)}
-            >
-            asdf
-              {/* <MaterialIcon icon="all_inclusive" invert /> */}
+            <Button className="btn-circle" onClick={(e) => this.props.catClick("parks", e)}>
+              <Fa icon="bicycle" />
             </Button>
-            <Button
-              className="btn-circle"
-              onClick={e => this.props.catClick("parks", e)}
-            >
-            asdf
-              {/* <MaterialIcon icon="local_florist" invert /> */}
-            </Button>
-            <Button
-              className="btn-circle"
-              onClick={e => this.props.catClick("swimming", e)}
-            >
-            asdf
-              {/* <MaterialIcon icon="local_bar" invert /> */}
-            </Button>
-            <Button
-              className="btn-circle"
-              onClick={e => this.props.catClick("hiking", e)}
-            >
-            asdf
-              {/* <MaterialIcon icon="terrain" invert /> */}
-            </Button>
+            {/* <Button className="btn-circle">
+              <Fa icon="users" />
+            </Button> */}
           </div>
         </div>
+
+        {/* <div>
+          <div className="save-location-btn">
+            <Button className="btn-large" onClick={this.toggle}>
+              +
+            </Button>
+            <Modal
+              isOpen={this.state.modal}
+              toggle={this.toggle}
+              size="fluid"
+              position="bottom"
+            >
+              <ModalHeader toggle={this.toggle} className="header" />
+              <ModalBody>
+                <div className="form-group">
+                  <label> Choose Category:</label>
+                  <select className="form-control">
+                    <option>Drinks</option>
+                    <option>Shopping</option>
+                    <option>Scenic</option>
+                    <option>Music</option>
+                    <option>Outdoors</option>
+                    <option>Misc</option>
+                  </select>
+                </div>
+                <br />
+                <label>Add Comments:</label>
+                <input
+                  type="textarea"
+                  className="form-control form-control-lg"
+                  label="Add Comments:"
+                />
+                <br />
+                <Button className="btn-large-modal btn-large-left">
+                  <Fa icon="camera-retro" size="2x" />
+                </Button>{" "}
+                <span className="add-photo"> Add Photo </span>
+              </ModalBody>
+              <ModalFooter className="footer">
+                <Button className="btn-large-modal" onClick={this.toggle}>
+                  <Fa icon="times" size="2x" />
+                </Button>{" "}
+                <Button className="btn-large-modal">
+                  <Fa icon="save" size="2x" />
+                </Button>
+              </ModalFooter>
+            </Modal>
+          </div>
+        </div> */}
       </div>
     );
   }
 }
 
-export default Toolbar;
+export default ToolbarFeatures;
